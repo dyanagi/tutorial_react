e8c431999b44f501d506b1c1cf147dbb83809389
Michael (Daichi) Yanagi <6637846+dyanagi@users.noreply.github.com>
2019-12-08 16:59:00 -0800

    Tutorial 29 Axios

--- README.md
+++ README.md
@@ -372,3 +372,68 @@ export default Rainbow(About);
 
 Read official documentation for more information.
 
+## 29 Axios
+
+```bash
+cd poketimes
+
+npm install axios
+```
+
+This app is using [### JSONPlaceHolder](https://jsonplaceholder.typicode.com/).
+
+```jsx harmony
+import React, { Component } from "react";
+import axios from 'axios'
+
+class Home extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    // `.then()` is called when the `get()` is completed
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res)
+
+        // (1) Set the state
+        this.setState({
+          posts: res.data.slice(0, 10)
+        })
+      })
+  }
+
+  render() {
+    // (2) Get the data from the state
+    const { posts } = this.state
+
+    // First show 'No posts yet',and then once the data is loaded, display the data.
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={ post.id }>
+            <div className="card-content">
+              <span className="card-title">
+                { post.title }
+              </span>
+              <p>{ post.body }</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    )
+
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        { postList }
+      </div>
+    )
+  }
+}
+
+export default Home;
+```
--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum magni odio voluptatibus! Assumenda, beatae dolor dolore, ex fugiat ipsa molestiae nobis porro reprehenderit soluta ullam veniam! Error quisquam repellendus tenetur.</p>
-    </div>
-  )
+class Home extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    // `.then()` is called when the `get()` is completed
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res)
+
+        // (1) Set the state
+        this.setState({
+          posts: res.data.slice(0, 10)
+        })
+      })
+  }
+
+  render() {
+    // (2) Get the data from the state
+    const { posts } = this.state
+
+    // First show 'No posts yet',and then once the data is loaded, display the data.
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={ post.id }>
+            <div className="card-content">
+              <span className="card-title">
+                { post.title }
+              </span>
+              <p>{ post.body }</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    )
+
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        { postList }
+      </div>
+    )
+  }
 }
 
 export default Home;
