09da0699c9d1e4ff130cb2ffc3b5c9b86464d9c7
Michael (Daichi) Yanagi <6637846+dyanagi@users.noreply.github.com>
2019-12-08 21:31:19 -0800

    Fix warning

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = (props) => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poke'Times</a>
+        <span className="brand-logo">Poke'Times</span>
         <ul className="right">
           <li><Link to="/">Home</Link></li>
           <li><NavLink to="/about">About</NavLink></li>
